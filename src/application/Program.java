dfa265efe136b9b88840d46e3715e747532cbde7
Valdecir Sarat <valdecir.sarat@gmail.com>
2022-02-11 18:57:51 -0300

    Implementação do findById

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,27 +1,19 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		Department obj = new Department(1, "Books");
-		
-		Seller seller = new Seller(21, "bob","bobgmail.com", new Date(), 3000.00,obj);
+	public static void main(String[] args) {		
 		
 		SellerDao sellerDao = DaoFactory.createSelerDao();
 		
+		Seller seller = sellerDao.findById(3);
+		
 		System.out.println(seller);
 		
-		
-
 	}
 
 }
