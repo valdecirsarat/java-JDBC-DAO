d3495d798542b604592b1e8b9b1ab9009e9ffbaa
Valdecir Sarat <valdecir.sarat@gmail.com>
2022-02-11 18:16:00 -0300

    Criando calsse SellerDaoJDBC e DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -13,7 +15,12 @@ public class Program {
 		Department obj = new Department(1, "Books");
 		
 		Seller seller = new Seller(21, "bob","bobgmail.com", new Date(), 3000.00,obj);
+		
+		SellerDao sellerDao = DaoFactory.createSelerDao();
+		
 		System.out.println(seller);
+		
+		
 
 	}
 
--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -8,7 +8,7 @@ public interface SellerDao {
 
 	void insert(Seller obj);
 	void update(Seller obj);
-	void deleById(Integer id);
+	void deleteById(Integer id);
 	Seller findById(Integer id);
 	
 	List<Seller> findAll();
